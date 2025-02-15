a66669239facbd04a9c101d662ca97239bdfb595
puttasrikant546 <puttasrikant185@gmail.com>
2025-02-15 18:55:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-# app.py
+from flask import Flask, render_template, request
 
-menu =  {
+app = Flask(__name__)
+
+menu = {
     'tatapunch': 613000,
     'hyundaicreta' : 2200000,
     'marutifronx' : 1300000,
@@ -14,40 +16,21 @@ menu =  {
     'lamborghini' : 300000000,
 }
 
-def welcome_message():
-    print("Welcome to Srikant's Super Luxury Car Showroom")
-    print("Check the menu of available cars in the showroom:")
-    print("Out of menu, no cars are available.")
-    print(f"You can take a loan or give money directly. The price will be reduced according to your wish.")
-    print("\nAvailable cars:")
-    for car, price in menu.items():
-        print(f"{car} = {price}")
+@app.route('/')
+def home():
+    return render_template('index.html', menu=menu)
 
-def order_car():
+@app.route('/order', methods=['POST'])
+def order():
+    item_1 = request.form['car1'].lower()
+    item_2 = request.form['car2'].lower() if 'car2' in request.form else None
+
     order_total = 0
-    item_1 = input("Enter the name of the car you want to order: ").lower()
     if item_1 in menu:
         order_total += menu[item_1]
-        print(f"Your car {item_1} has been added to your order.")
-        payment_method = input("Do you want to take a loan or pay directly? (loan/direct): ").lower()
-        print(f"Your car warranty is for three years. Any problems? Free service is available.")
-    else:
-        print(f"Order item {item_1} is not available!")
-    
-    another_order = input("Do you want to add another car? (yes/no): ").lower()
-    if another_order == "yes":
-        item_2 = input("Enter the name of the second car you want to order: ").lower()
-        if item_2 in menu:
-            order_total += menu[item_2]
-            print(f"Your car {item_2} has been added to your order.")
-            print(f"Your car warranty is for three years. Any problems? Free service is available.")
-        else:
-            print(f"Order item {item_2} is not available!")
-    else:
-        print("No second car added to your order.")
-    
-    print(f"Total cost of your order: {order_total}")
+    if item_2 in menu:
+        order_total += menu[item_2]
+    return f"Total cost of your order: {order_total}"
 
-# Main function to start the process
-welcome_message()
-order_car()
+if __name__ == "__main__":
+    app.run(debug=True)
